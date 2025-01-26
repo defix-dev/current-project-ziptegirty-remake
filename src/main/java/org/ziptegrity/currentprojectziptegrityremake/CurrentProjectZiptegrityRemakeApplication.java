9bbafa3f3e96945093d18fc781f5ab3d96af74ae
defix-dev <kostikmelnik7778@gmail.com>
2025-01-26 14:32:30 +0300

    Start

---
+++ src/main/java/org/ziptegrity/currentprojectziptegrityremake/CurrentProjectZiptegrityRemakeApplication.java
@@ -0,0 +1,13 @@
+package org.ziptegrity.currentprojectziptegrityremake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrentProjectZiptegrityRemakeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrentProjectZiptegrityRemakeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ziptegrity/currentprojectziptegrityremake/CurrentProjectZiptegrityRemakeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ziptegrity.currentprojectziptegrityremake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrentProjectZiptegrityRemakeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
